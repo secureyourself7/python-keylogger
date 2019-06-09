ce8258f9514c2549f45e16ebd79459f42000b264
Ricardo Garcia <51318845+secureyourself7@users.noreply.github.com>
2019-06-09 04:47:09 +0000

    Update rsa_key_generator.py

--- rsa_key_generator.py
+++ rsa_key_generator.py
@@ -11,12 +11,17 @@ full_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 # - GLOBAL SCOPE VARIABLES end -
 
 # GENERATE NEW RSA PUBLIC-PRIVATE KEY PAIR:
-passphrase = getpass.getpass(prompt='Enter your private key passphrase')
+passphrase_1 = getpass.getpass(prompt='Enter your private key passphrase (your input will be hidden):\n')
+passphrase_2 = getpass.getpass(prompt='Confirm your private key passphrase (your input will be hidden):\n')
+if passphrase_1 != passphrase_2:
+    raise ValueError("Your inputs don't match!")
+print('Inputs match, generating your keys...')
 new_key = RSA.generate(4096)
 export_format = 'PEM'
-private_key = new_key.exportKey(pkcs=8, passphrase=passphrase)
+private_key = new_key.exportKey(pkcs=8, passphrase=passphrase_1)
 public_key = new_key.publickey().exportKey(format=export_format)
 with open(os.path.join(full_path, "private_key." + export_format.lower()), "wb") as f:
     f.write(private_key)
 with open(os.path.join(full_path, "public_key." + export_format.lower()), "wb") as f:
     f.write(public_key)
+print('Finished!')
